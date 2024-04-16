96cf02a7189d2db454a69f6f565aced0b71eeda1
Prisha Srinidi <prishasrinidi@gmail.com>
2024-04-16 09:18:51 +0530

    more search routes added

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -10,4 +10,69 @@ const search = (app, db) => {
       }
     });
   });
+
+  app.get("/login/user/package/:pname", (req, res) => {
+    const pname = req.params.pname;
+    const sqlSelect = "select * from package where name=?";
+    db.query(sqlSelect, pname, (err, result) => {
+      if (err) {
+        console.log("**ERROR**" + err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/login/user/package/:dname", (req, res) => {
+    const dname = req.params.dname;
+    const sqlSelect = "select * from package where destination=?";
+    db.query(sqlSelect, dname, (err, result) => {
+      if (err) {
+        console.log("**ERROR**" + err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/login/user/package/:amt", (req, res) => {
+    const amt = req.params.amt;
+    const sqlSelect = "select * from package where cost<=?";
+    db.query(sqlSelect, amt, (err, result) => {
+      if (err) {
+        console.log("**ERROR**" + err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/login/user/package/:dur", (req, res) => {
+    const dur = req.params.dur;
+    const sqlSelect = "select * from package where dur<=?";
+    db.query(sqlSelect, dur, (err, result) => {
+      if (err) {
+        console.log("**ERROR**" + err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      }
+    });
+  });
+
+  app.get("/login/user/flight/:pid", (req, res) => {
+    const pid = req.params.pid;
+    const sqlSelect = "select * from flight f, package_details p where p.";
+    db.query(sqlSelect, pname, (err, result) => {
+      if (err) {
+        console.log("**ERROR**" + err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      }
+    });
+  });
 };
